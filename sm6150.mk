c08a462963015a044ea4eb32d3d240fdb7e0f36e
Omkar Chandorkar <forumomkar@gmail.com>
2023-04-02 19:13:28 +0700

    sm6150-common: wlan: Enable IEEE 802.11ax support
    IEEE80211AX config is enabled inside qcom proprietary on recent QC SOCs starting from msmnile (855+)
    So, add support for building IEEE80211AX support inside wpa_supplicant.
    
    Below commit enables 11ax configuration on kona
    https://git.codelinaro.org/clo/la/platform/vendor/qcom/wlan/-/commit/0113651d8ce088fe133fce9a608ba13d7fb4b8c3
    As lahaina hostapd configuration is based on kona hence, enable 11ax support on kona and lahaina.
    
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>
    Change-Id: I9432172a9f8916f0d606416886c9da3ed5514f38

--- sm6150.mk
+++ sm6150.mk
@@ -412,6 +412,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
+# Wifi Enable IEEE 802.11ax support
+WIFI_FEATURE_HOSTAPD_11AX := true
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
